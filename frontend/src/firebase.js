a3d9024a0d51dfaa703f49af5dfb5fd6be63b934
Mahra <mahra@Mahras-MacBook-Pro.local>
2026-05-03 01:26:07 +0400

    Clean repo and update Firebase config

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,14 +1,19 @@
-// src/firebase.js
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
-import firebaseConfig from '../../root/shared/firebaseConfig';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-const app = initializeApp({
-  ...firebaseConfig,
+// Use environment variables
+const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-});
-const db = getFirestore(app);
-const auth = getAuth(app);
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
 
-export { db, auth };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { app, analytics };
